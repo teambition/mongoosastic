d909c41caa5c2148a881b17e79fe7f7351564f3b
isayme <isaymeorg@gmail.com>
2015-07-08 18:25:31 +0800

    add reverse query hook & refine test

--- test/subdoc-search-test.js
+++ test/subdoc-search-test.js
@@ -7,16 +7,28 @@ var mongoose = require('mongoose'),
   mongoosastic = require('../lib/mongoosastic');
 
 var AuthorSchema = new Schema({
-  name: {type: String, es_indexed: true}
-})
+  name: {
+    type: String,
+    es_indexed: true
+  }
+});
+AuthorSchema.set('versionKey', false);
 
 var BookSchema = new Schema({
-  title: {type: String, es_indexed: true},
-  author_id: {type: Schema.Types.ObjectId, ref: 'Author', es_schema: AuthorSchema, es_indexed: true},
-  authors_ids: [
-    {type: Schema.Types.ObjectId, ref: 'Author', es_schema: AuthorSchema, es_indexed: true}
+  title: {
+    type: String,
+    es_indexed: true
+  },
+  authors: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Author',
+      es_schema: AuthorSchema,
+      es_indexed: true
+    }
   ]
-})
+});
+BookSchema.set('versionKey', false);
 
 BookSchema.plugin(mongoosastic, {
   index: 'es-index',
@@ -29,76 +41,99 @@ BookSchema.plugin(mongoosastic, {
 var Book = mongoose.model('Book', BookSchema)
 var Author = mongoose.model('Author', AuthorSchema)
 
-describe('Subdoc configure with es_schema', function() {
+describe('Feature: reIndex document if subdocument changed!', function() {
   before(function(done) {
-    mongoose.connect(config.mongoUrl, function() {
-      Book.remove(function() {
-        Author.remove(function() {
-          config.deleteIndexIfExists(['es-index'], function() {
-            var author = new Author({name: 'author name'});
+    async.series([
+      function(next) {
+        mongoose.connect(config.mongoUrl, next);
+      },
+      function(next) {
+        config.deleteIndexIfExists(['es-index'], next);
+      },
+      function(next) {
+        Book.createMapping(next);
+      }
+    ], done)
+  });
 
-            var book = new Book({title: 'book title'});
-            book.author_id = author;
+  after(function(done) {
+    async.parallel([
+      function(next) {
+        mongoose.disconnect(next);
+      },
+      function(next) {
+        config.deleteIndexIfExists(['es-index'], next);
+      }
+    ], done)
+    
+  })
 
-            author.save(function() {
-              book.save(function() {
-                setTimeout(done, 2000);
-              });
-            })
-          });
+  describe('subdocument configure with es_schema', function() {
+    var author, book;
+
+    before(function(done) {
+      async.parallel([
+        function(next) {
+          author = new Author({name: 'author name'});
+          author.save(next);
+        },
+        function(next) {
+          book = new Book({title: 'book title'});
+          next(null, book, 1);
+        }
+      ], function(err, results) {
+        if (err) {
+          return done(err);
+        }
+
+        book.authors.push(author);
+        book.save();
+
+        book.once('es-indexed', done);
+      })
+    });
+
+    it('should find subdoc added', function(done) {
+      setTimeout(function() {
+        Book.search(undefined, function(err, results) {
+          results.hits.total.should.eql(1);
+          results.hits.hits[0]._source.authors.length.should.eql(1);
+          results.hits.hits[0]._source.authors[0].name.should.eql('author name');
+
+          done()
         })
+      }, 1000)
+    });
+
+    it('should find more subdocs after add anthor subdocument', function(done) {
+
+      var another = new Author({name: 'newname'});
+
+      async.series([
+        function(next) {
+            another.save(next);
+        },
+        function(next) {
+          book.authors.push(another);
+          book.save(next)
+        }
+      ], function(err) {
+        if (err) {
+          return done(err);
+        }
+
+        setTimeout(function() {
+          Book.search(undefined, function(err, results) {
+            results.hits.total.should.eql(1);
+            results.hits.hits[0]._source.authors.length.should.eql(2);
+            results.hits.hits[0]._source.authors[1].name.should.eql('newname');
+
+            done()
+          });
+        }, 1000)
+
       });
     });
   });
 
-  after(function() {
-    mongoose.disconnect();
-  });
-
-  it('should not find subdoc', function(done) {
-    Book.search(undefined, function(err, results) {
-      results.hits.total.should.eql(1);
-      results.hits.hits[0]._source.author_id.name.should.eql('author name');
-
-      done()
-    })
-  });
-});
-
-
-
-describe('Subdoc Array configure with es_schema', function() {
-  before(function(done) {
-    mongoose.connect(config.mongoUrl, function() {
-      Book.remove(function() {
-        Author.remove(function() {
-          config.deleteIndexIfExists(['es-index'], function() {
-            var author = new Author({name: 'author name'});
-
-            var book = new Book({title: 'book title'});
-            book.authors_ids.push(author);
-
-            author.save(function() {
-              book.save(function() {
-                setTimeout(done, 2000);
-              });
-            })
-          });
-        })
-      });
-    });
-  });
-
-  after(function() {
-    mongoose.disconnect();
-  });
-
-  it('should not find subdoc', function(done) {
-    Book.search(undefined, function(err, results) {
-      results.hits.total.should.eql(1);
-      results.hits.hits[0]._source.authors_ids[0].name.should.eql('author name');
-
-      done()
-    })
-  });
 });
